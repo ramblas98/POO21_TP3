8d7ee1ca079da505e8bca00e05b30f4982a8ce3f
Antonio <Antonio@Antonio-PC>
2021-06-07 12:08:37 -0300

    Ejercicio 2 Tp3 II

--- POO_TP3_EJ2/src/poo_tp3_ej2/POO_TP3_EJ2.java
+++ POO_TP3_EJ2/src/poo_tp3_ej2/POO_TP3_EJ2.java
@@ -4,7 +4,9 @@ package poo_tp3_ej2;
 public class POO_TP3_EJ2 {
 
     public static void main(String[] args) {
-    
+        Autobus autobus1 = new Autobus(42, 25.76, 11, 5649, "Mercedes Benz");
+        
+        System.out.println("La recaudacion total de Autobus 1 es :"+autobus1.recaudacionTotal());
     }
     
 }
